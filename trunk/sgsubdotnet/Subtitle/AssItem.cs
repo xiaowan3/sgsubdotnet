53ef140613bdbb01d168d930bf7294275f86db15
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2010-11-18 15:10:52 +0000

    Read Ass

--- trunk/sgsubdotnet/Subtitle/AssItem.cs
+++ trunk/sgsubdotnet/Subtitle/AssItem.cs
@@ -5,21 +5,46 @@ using System.Text;
 
 namespace Subtitle
 {
+    public class AssHead
+    {
+        public List<string> m_HeadLines = new List<string>();
+
+        public void AddLine(string line)
+        {
+            m_HeadLines.Add(line);
+        }
+
+        public void Clear()
+        {
+            m_HeadLines = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (string s in m_HeadLines)
+                str += s;
+            return str;
+        }
+    }
     public class AssItem
     {
         //Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
-        public string Format;
+        public string Format = "";
 
-        public string Marked;
+        public string Marked = "";
+
+        public string Layer = "";
 
         public string StartTime
         {
             get
             {
-                return "";
+                return Start.ToString();
             }
             set
             {
+                Start = new AssTime(value);
             }
         }
 
@@ -27,30 +52,36 @@ namespace Subtitle
         {
             get
             {
-                return "";
+                return End.ToString();
             }
             set
             {
+                End = new AssTime(value);
             }
         }
 
-        public AssTime Start;
+        public AssTime Start = new AssTime();
 
-        public AssTime End;
+        public AssTime End = new AssTime();
 
-        public string Style;
+        public string Style = "";
 
-        public string Name;
+        public string Name = "";
 
-        public int MarginL;
+        public int MarginL = 0;
 
-        public int MarginR;
+        public int MarginR = 0;
 
-        public int MarginV;
+        public int MarginV = 0;
 
-        public string Effect;
+        public string Effect = "";
 
-        public string Text;
+        private string m_Text = "";
+        public string Text
+        {
+            get { return m_Text; }
+            set { m_Text = value; }
+        }
 
         public AssItem()
         {
@@ -60,35 +91,170 @@ namespace Subtitle
 
     public class AssTime
     {
-        public AssTime()
+        double timeValue;
+
+        public double TimeValue
         {
+            get
+            {
+                return timeValue;
+            }
         }
 
-        public AssTime(string Time)
+        public AssTime()
         {
+            timeValue = 0;
+        }
+
+        public AssTime(string time)
+        {
+            string[] s = new string[3];
+            int c = 0;
+            for (int i = 0; i < time.Length; i++)
+            {
+                if (Char.IsNumber(time[i]) || time[i] == '.')
+                {
+                    s[c] += time[i];
+                }
+                else
+                {
+                    if (time[i] == ':')
+                        c++;
+                }
+            }
+            timeValue = int.Parse(s[0]) * 3600 + int.Parse(s[1]) * 60 + float.Parse(s[2]);
+
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            int h, m,s,ms;
+            int intTime = (int)Math.Round(timeValue * 100);
+            h = intTime / 360000;
+            intTime %= 360000;
+            m = intTime / 6000;
+            intTime %= 6000;
+            s = intTime / 100;
+            ms = intTime % 100;
+            return h.ToString() + ":" + m.ToString("D2") + ":" + s.ToString("D2") + "." + ms.ToString("D2");
         }
     }
     //Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 
-    class AssLineParser
+    public class AssLineParser
     {
-        private string[] spliter = new string[10];
-        private int formatpos;
-        private int markedpos;
-        private int startpos;
-        private int endpos;
-        private int stylepos;
-        private int namepos;
-        private int marginlpos;
-        private int marginrpos;
-        private int marginvpos;
-        private int effectpos;
-        private int textpos;
+        private char[] spliter = new char[12];
+        private int formatpos = -1;
+        private int markedpos = -1;
+        private int startpos = -1;
+        private int endpos = -1;
+        private int stylepos = -1;
+        private int namepos = -1;
+        private int marginlpos = -1;
+        private int marginrpos = -1;
+        private int marginvpos = -1;
+        private int effectpos = -1;
+        private int textpos = -1;
+        private int layerPos = -1;
+        public AssLineParser(string format)
+        {
+            string seg = "";
+            int pos = 0;
+            int s = 0;
+            format += "#";
+            for(int i = 0;i<format.Length;i++)
+            {
+                if (Char.IsLetter(format[i])) seg += format[i];
+                else
+                {
+                    if (!Char.IsSeparator(format[i]))
+                    {
+                        switch (seg.ToUpper())
+                        {
+                            case "FORMAT":
+                                formatpos = pos;
+                                break;
+                            case "MARKED":
+                                markedpos = pos;
+                                break;
+                            case "LAYER":
+                                layerPos = pos;
+                                break;
+                            case "START":
+                                startpos = pos;
+                                break;
+                            case "END":
+                                endpos = pos;
+                                break;
+                            case "STYLE":
+                                stylepos = pos;
+                                break;
+                            case "NAME":
+                                namepos = pos;
+                                break;
+                            case "MARGINL":
+                                marginlpos = pos;
+                                break;
+                            case "MARGINR":
+                                marginrpos = pos;
+                                break;
+                            case "MARGINV":
+                                marginvpos = pos;
+                                break;
+                            case "EFFECT":
+                                effectpos = pos;
+                                break;
+                            case "TEXT":
+                                textpos = pos;
+                                break;
+                            default:
+                                break;
+                        }
+                        spliter[s] = format[i];
+                        s++;
+                        pos++;
+                        seg = "";
+                    }
+                }
+            }
+        }
+
+        public AssItem ParseLine(string line)
+        {
+            string[] segs = new string[12];
+            int last = 0;
+            int len;
+            for (int i = 0; i < 12; i++)
+            {
+                if (spliter[i] != '#')
+                {
+                    len = line.IndexOf(spliter[i], last) - last;
+                    segs[i] = line.Substring(last, len);
+                    last = len + last + 1;
+                }
+                else
+                {
+                    segs[i] = line.Substring(last);
+                    break;
+                }
+
+            }
+            AssItem assitem = new AssItem();
+            if (formatpos != -1) assitem.Format = segs[formatpos];
+            if (markedpos != -1) assitem.Marked = segs[markedpos];
+            if (layerPos != -1) assitem.Layer = segs[layerPos];
+            if (startpos != -1) assitem.StartTime = segs[startpos];
+            if (endpos != -1) assitem.EndTime = segs[endpos];
+            if (stylepos != -1) assitem.Style = segs[stylepos];
+            if (namepos != -1) assitem.Name = segs[namepos];
+            if (marginlpos != -1) assitem.MarginL = int.Parse(segs[marginlpos]);
+            if (marginrpos != -1) assitem.MarginR = int.Parse(segs[marginrpos]);
+            if (marginvpos != -1) assitem.MarginV = int.Parse(segs[marginvpos]);
+            if (effectpos != -1) assitem.Effect = segs[effectpos];
+            if (textpos != -1) assitem.Text = segs[textpos];
+
+            return assitem;
+        }
 
     }
 
--- trunk/sgsubdotnet/sgsubdotnet/SGSMainForm.Designer.cs
+++ trunk/sgsubdotnet/sgsubdotnet/SGSMainForm.Designer.cs
@@ -30,21 +30,23 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SGSMainForm));
             this.hSpliter = new System.Windows.Forms.SplitContainer();
-            this.mainMenu = new System.Windows.Forms.MenuStrip();
-            this.statusBar = new System.Windows.Forms.StatusStrip();
             this.vSpliter = new System.Windows.Forms.SplitContainer();
             this.axWMP = new AxWMPLib.AxWindowsMediaPlayer();
-            this.ssubtitleGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             this.smallSpliter = new System.Windows.Forms.SplitContainer();
+            this.subtitleGrid = new System.Windows.Forms.DataGridView();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
             this.hSpliter.Panel1.SuspendLayout();
             this.hSpliter.Panel2.SuspendLayout();
             this.hSpliter.SuspendLayout();
             this.vSpliter.Panel1.SuspendLayout();
+            this.vSpliter.Panel2.SuspendLayout();
             this.vSpliter.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWMP)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ssubtitleGrid)).BeginInit();
             this.smallSpliter.Panel2.SuspendLayout();
             this.smallSpliter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.subtitleGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // hSpliter
@@ -65,6 +67,69 @@
             this.hSpliter.SplitterDistance = 328;
             this.hSpliter.TabIndex = 0;
             // 
+            // vSpliter
+            // 
+            this.vSpliter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vSpliter.Location = new System.Drawing.Point(0, 0);
+            this.vSpliter.Name = "vSpliter";
+            // 
+            // vSpliter.Panel1
+            // 
+            this.vSpliter.Panel1.Controls.Add(this.axWMP);
+            // 
+            // vSpliter.Panel2
+            // 
+            this.vSpliter.Panel2.Controls.Add(this.button1);
+            this.vSpliter.Size = new System.Drawing.Size(734, 328);
+            this.vSpliter.SplitterDistance = 382;
+            this.vSpliter.TabIndex = 0;
+            // 
+            // axWMP
+            // 
+            this.axWMP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWMP.Enabled = true;
+            this.axWMP.Location = new System.Drawing.Point(0, 0);
+            this.axWMP.Name = "axWMP";
+            this.axWMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWMP.OcxState")));
+            this.axWMP.Size = new System.Drawing.Size(382, 328);
+            this.axWMP.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // smallSpliter
+            // 
+            this.smallSpliter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.smallSpliter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.smallSpliter.IsSplitterFixed = true;
+            this.smallSpliter.Location = new System.Drawing.Point(0, 0);
+            this.smallSpliter.Name = "smallSpliter";
+            this.smallSpliter.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // smallSpliter.Panel2
+            // 
+            this.smallSpliter.Panel2.Controls.Add(this.subtitleGrid);
+            this.smallSpliter.Size = new System.Drawing.Size(734, 227);
+            this.smallSpliter.SplitterDistance = 45;
+            this.smallSpliter.TabIndex = 1;
+            // 
+            // subtitleGrid
+            // 
+            this.subtitleGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.subtitleGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.subtitleGrid.Location = new System.Drawing.Point(0, 0);
+            this.subtitleGrid.Name = "subtitleGrid";
+            this.subtitleGrid.RowTemplate.Height = 23;
+            this.subtitleGrid.Size = new System.Drawing.Size(734, 178);
+            this.subtitleGrid.TabIndex = 0;
+            // 
             // mainMenu
             // 
             this.mainMenu.Location = new System.Drawing.Point(0, 0);
@@ -81,55 +146,6 @@
             this.statusBar.TabIndex = 2;
             this.statusBar.Text = "Status";
             // 
-            // vSpliter
-            // 
-            this.vSpliter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.vSpliter.Location = new System.Drawing.Point(0, 0);
-            this.vSpliter.Name = "vSpliter";
-            // 
-            // vSpliter.Panel1
-            // 
-            this.vSpliter.Panel1.Controls.Add(this.axWMP);
-            this.vSpliter.Size = new System.Drawing.Size(734, 328);
-            this.vSpliter.SplitterDistance = 382;
-            this.vSpliter.TabIndex = 0;
-            // 
-            // axWMP
-            // 
-            this.axWMP.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.axWMP.Enabled = true;
-            this.axWMP.Location = new System.Drawing.Point(0, 0);
-            this.axWMP.Name = "axWMP";
-            this.axWMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWMP.OcxState")));
-            this.axWMP.Size = new System.Drawing.Size(382, 328);
-            this.axWMP.TabIndex = 0;
-            // 
-            // ssubtitleGrid
-            // 
-            this.ssubtitleGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ssubtitleGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ssubtitleGrid.Location = new System.Drawing.Point(0, 0);
-            this.ssubtitleGrid.Name = "ssubtitleGrid";
-            this.ssubtitleGrid.RowTemplate.Height = 23;
-            this.ssubtitleGrid.Size = new System.Drawing.Size(734, 178);
-            this.ssubtitleGrid.TabIndex = 0;
-            // 
-            // smallSpliter
-            // 
-            this.smallSpliter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.smallSpliter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.smallSpliter.IsSplitterFixed = true;
-            this.smallSpliter.Location = new System.Drawing.Point(0, 0);
-            this.smallSpliter.Name = "smallSpliter";
-            this.smallSpliter.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // smallSpliter.Panel2
-            // 
-            this.smallSpliter.Panel2.Controls.Add(this.ssubtitleGrid);
-            this.smallSpliter.Size = new System.Drawing.Size(734, 227);
-            this.smallSpliter.SplitterDistance = 45;
-            this.smallSpliter.TabIndex = 1;
-            // 
             // SGSMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -144,11 +160,12 @@
             this.hSpliter.Panel2.ResumeLayout(false);
             this.hSpliter.ResumeLayout(false);
             this.vSpliter.Panel1.ResumeLayout(false);
+            this.vSpliter.Panel2.ResumeLayout(false);
             this.vSpliter.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.axWMP)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ssubtitleGrid)).EndInit();
             this.smallSpliter.Panel2.ResumeLayout(false);
             this.smallSpliter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.subtitleGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,8 +178,9 @@
         private System.Windows.Forms.StatusStrip statusBar;
         private System.Windows.Forms.SplitContainer vSpliter;
         private AxWMPLib.AxWindowsMediaPlayer axWMP;
-        private System.Windows.Forms.DataGridView ssubtitleGrid;
+        private System.Windows.Forms.DataGridView subtitleGrid;
         private System.Windows.Forms.SplitContainer smallSpliter;
+        private System.Windows.Forms.Button button1;
     }
 }
 
