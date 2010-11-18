2337d6042e1ecc00b85c63a18efdf8e2a8bf884e
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2010-11-18 15:10:52 +0000

    Read Ass

--- sgsubdotnet/Subtitle/AssSub.cs
+++ sgsubdotnet/Subtitle/AssSub.cs
@@ -2,10 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Subtitle
 {
     public class AssSub
     {
+        private AssHead m_AssHead;
+        private AssHead m_DefaultHead;
+        private AssLineParser m_AssParser;
+        public BindingSource SubItems = new BindingSource();
+
+        public void LoadAss(string filename)
+        {
+            FileStream ifile = new FileStream(filename, FileMode.Open);
+            StreamReader istream = new StreamReader(ifile);
+            LoadAss(istream);
+            istream.Close();
+            ifile.Close();
+        }
+        public void LoadAss(string filename, Encoding encoding)
+        {
+            FileStream ifile = new FileStream(filename, FileMode.Open);
+            StreamReader istream = new StreamReader(ifile,encoding);
+            LoadAss(istream);
+            istream.Close();
+            ifile.Close();
+        }
+
+        public void LoadAss(StreamReader iStream)
+        {
+            string line;
+            bool eventfound = false;
+            m_AssHead = new AssHead();
+
+            while (!iStream.EndOfStream)
+            {
+                line = iStream.ReadLine();
+                m_AssHead.AddLine(line);
+                if (line.ToUpper().IndexOf("EVENTS") != -1)
+                {
+                    eventfound = true;
+                    break;
+                }
+            }
+            line = iStream.ReadLine();
+            m_AssParser = new AssLineParser(line);
+            while (!iStream.EndOfStream)
+            {
+                line = iStream.ReadLine();
+                SubItems.Add(m_AssParser.ParseLine(line));
+            }
+        }
+
+        public void LoadText(string filename)
+        {
+        }
+        public void LoadText(string filename, Encoding encoding)
+        {
+        }
     }
 }
--- sgsubdotnet/sgsubdotnet/SGSMainForm.cs
+++ sgsubdotnet/sgsubdotnet/SGSMainForm.cs
@@ -15,5 +15,35 @@ namespace sgsubdotnet
         {
             InitializeComponent();
         }
+
+        private Subtitle.AssSub sub = new Subtitle.AssSub();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string formatline = "Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text";
+            string linetoparse = "Dialogue: 0,0:01:11.05,0:01:15.23,*Default,NTP,0000,0000,0000,,好吃就行了管他的呢 再来一碗";
+            Subtitle.AssLineParser parser = new Subtitle.AssLineParser(formatline);
+      
+            parser.ParseLine(linetoparse);
+
+            DataGridViewColumn column;
+            sub.LoadAss("E:\\test\\ass.ass");
+            subtitleGrid.AutoGenerateColumns = false;
+            subtitleGrid.AutoSize = false;
+
+            subtitleGrid.DataSource = sub.SubItems;
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "StartTime";
+            subtitleGrid.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "EndTime";
+            subtitleGrid.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Text";
+            subtitleGrid.Columns.Add(column);
+        }
     }
 }
