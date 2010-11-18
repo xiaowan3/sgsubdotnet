d3e74481cdb77046df8b3d031fb07cab6454678b
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2010-11-18 10:34:45 +0000

    Draw UI

--- trunk/sgsubdotnet/Subtitle/AssItem.cs
+++ trunk/sgsubdotnet/Subtitle/AssItem.cs
@@ -5,7 +5,92 @@ using System.Text;
 
 namespace Subtitle
 {
-    class AssItem
+    public class AssItem
     {
+        //Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+        public string Format;
+
+        public string Marked;
+
+        public string StartTime
+        {
+            get
+            {
+                return "";
+            }
+            set
+            {
+            }
+        }
+
+        public string EndTime
+        {
+            get
+            {
+                return "";
+            }
+            set
+            {
+            }
+        }
+
+        public AssTime Start;
+
+        public AssTime End;
+
+        public string Style;
+
+        public string Name;
+
+        public int MarginL;
+
+        public int MarginR;
+
+        public int MarginV;
+
+        public string Effect;
+
+        public string Text;
+
+        public AssItem()
+        {
+        }
+
     }
+
+    public class AssTime
+    {
+        public AssTime()
+        {
+        }
+
+        public AssTime(string Time)
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    //Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+
+    class AssLineParser
+    {
+        private string[] spliter = new string[10];
+        private int formatpos;
+        private int markedpos;
+        private int startpos;
+        private int endpos;
+        private int stylepos;
+        private int namepos;
+        private int marginlpos;
+        private int marginrpos;
+        private int marginvpos;
+        private int effectpos;
+        private int textpos;
+
+    }
+
+
 }
