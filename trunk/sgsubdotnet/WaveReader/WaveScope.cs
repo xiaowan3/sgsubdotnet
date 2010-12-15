cafe8c88c04e25b48cd23a58ca3c5f95b1b68ede
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2010-12-15 11:03:10 +0000

    波形显示。

---
+++ trunk/sgsubdotnet/WaveReader/WaveScope.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaveReader
+{
+    public partial class WaveScope : UserControl
+    {
+        public double CurrentPosition { get; set; }
+        public WaveForm Wave = null;
+        public WaveScope()
+        {
+            InitializeComponent();
+        }
+        private Pen bgPen = new Pen(Color.Black);
+        private Pen gridPen = new Pen(Color.Red);
+        private Pen linePen = new Pen(Color.Blue);
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            Redraw();
+        }
+        public void Redraw()
+        {
+            Graphics g1 = CreateGraphics();
+            BufferedGraphicsContext b = new BufferedGraphicsContext();
+            BufferedGraphics bg = b.Allocate(g1, new Rectangle(0, 0, Width, Height));
+
+            Graphics g = bg.Graphics;
+            g.Clear(Color.Black);
+            int startpos = (int)(Width * 0.75);
+            if (Wave != null)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    int v = (int)(50 * Wave.ValueAt((i - startpos) * Wave.DeltaT + CurrentPosition));
+                    g.DrawLine(linePen, i, 50 - v, i, 50 + v);
+                }
+                g.DrawLine(gridPen, startpos, 0, startpos, Height);
+            }
+            bg.Render(g1);
+        }
+    }
+}
