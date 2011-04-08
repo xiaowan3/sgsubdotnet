600754903c254d3e737d675fd71aed6f4b985cda
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2011-04-08 14:55:23 +0000

    SGS Controls.

---
+++ branches/TitaRussell/sgsubdotnet/SGSControls/SubEditor.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SGSControls
+{
+    partial class SubEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subeditorSpliter = new System.Windows.Forms.SplitContainer();
+            this.labelSub = new System.Windows.Forms.Label();
+            this.subEditorToolStrip = new System.Windows.Forms.ToolStripContainer();
+            this.dataGridSubtitles = new System.Windows.Forms.DataGridView();
+            this.subeditorSpliter.Panel1.SuspendLayout();
+            this.subeditorSpliter.Panel2.SuspendLayout();
+            this.subeditorSpliter.SuspendLayout();
+            this.subEditorToolStrip.ContentPanel.SuspendLayout();
+            this.subEditorToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSubtitles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // subeditorSpliter
+            // 
+            this.subeditorSpliter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.subeditorSpliter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.subeditorSpliter.Location = new System.Drawing.Point(0, 0);
+            this.subeditorSpliter.Name = "subeditorSpliter";
+            this.subeditorSpliter.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // subeditorSpliter.Panel1
+            // 
+            this.subeditorSpliter.Panel1.Controls.Add(this.labelSub);
+            // 
+            // subeditorSpliter.Panel2
+            // 
+            this.subeditorSpliter.Panel2.Controls.Add(this.subEditorToolStrip);
+            this.subeditorSpliter.Size = new System.Drawing.Size(456, 414);
+            this.subeditorSpliter.SplitterDistance = 44;
+            this.subeditorSpliter.TabIndex = 0;
+            // 
+            // labelSub
+            // 
+            this.labelSub.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelSub.AutoSize = true;
+            this.labelSub.Font = new System.Drawing.Font("SimSun", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSub.Location = new System.Drawing.Point(21, 12);
+            this.labelSub.Name = "labelSub";
+            this.labelSub.Size = new System.Drawing.Size(69, 20);
+            this.labelSub.TabIndex = 0;
+            this.labelSub.Text = "label1";
+            // 
+            // subEditorToolStrip
+            // 
+            // 
+            // subEditorToolStrip.ContentPanel
+            // 
+            this.subEditorToolStrip.ContentPanel.Controls.Add(this.dataGridSubtitles);
+            this.subEditorToolStrip.ContentPanel.Size = new System.Drawing.Size(456, 341);
+            this.subEditorToolStrip.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.subEditorToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.subEditorToolStrip.Name = "subEditorToolStrip";
+            this.subEditorToolStrip.Size = new System.Drawing.Size(456, 366);
+            this.subEditorToolStrip.TabIndex = 0;
+            this.subEditorToolStrip.Text = "toolStripContainer1";
+            // 
+            // dataGridSubtitles
+            // 
+            this.dataGridSubtitles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSubtitles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridSubtitles.Location = new System.Drawing.Point(0, 0);
+            this.dataGridSubtitles.Name = "dataGridSubtitles";
+            this.dataGridSubtitles.RowTemplate.Height = 23;
+            this.dataGridSubtitles.Size = new System.Drawing.Size(456, 341);
+            this.dataGridSubtitles.TabIndex = 0;
+            // 
+            // SubEditor
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.subeditorSpliter);
+            this.Name = "SubEditor";
+            this.Size = new System.Drawing.Size(456, 414);
+            this.subeditorSpliter.Panel1.ResumeLayout(false);
+            this.subeditorSpliter.Panel1.PerformLayout();
+            this.subeditorSpliter.Panel2.ResumeLayout(false);
+            this.subeditorSpliter.ResumeLayout(false);
+            this.subEditorToolStrip.ContentPanel.ResumeLayout(false);
+            this.subEditorToolStrip.ResumeLayout(false);
+            this.subEditorToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSubtitles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer subeditorSpliter;
+        private System.Windows.Forms.Label labelSub;
+        private System.Windows.Forms.ToolStripContainer subEditorToolStrip;
+        private System.Windows.Forms.DataGridView dataGridSubtitles;
+    }
+}
---
+++ branches/TitaRussell/sgsubdotnet/SGSControls/WaveFormViewer.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SGSControls
+{
+    partial class WaveFormViewer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripWaveFormViewer = new System.Windows.Forms.ToolStripContainer();
+            this.waveViewerLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.waveScope1 = new WaveReader.WaveScope();
+            this.toolStripWaveFormViewer.ContentPanel.SuspendLayout();
+            this.toolStripWaveFormViewer.SuspendLayout();
+            this.waveViewerLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripWaveFormViewer
+            // 
+            // 
+            // toolStripWaveFormViewer.ContentPanel
+            // 
+            this.toolStripWaveFormViewer.ContentPanel.Controls.Add(this.waveViewerLayout);
+            this.toolStripWaveFormViewer.ContentPanel.Size = new System.Drawing.Size(514, 220);
+            this.toolStripWaveFormViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripWaveFormViewer.Location = new System.Drawing.Point(0, 0);
+            this.toolStripWaveFormViewer.Name = "toolStripWaveFormViewer";
+            this.toolStripWaveFormViewer.Size = new System.Drawing.Size(514, 245);
+            this.toolStripWaveFormViewer.TabIndex = 0;
+            this.toolStripWaveFormViewer.Text = "toolStripContainer1";
+            // 
+            // waveViewerLayout
+            // 
+            this.waveViewerLayout.ColumnCount = 3;
+            this.waveViewerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.waveViewerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.waveViewerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.waveViewerLayout.Controls.Add(this.waveScope1, 0, 0);
+            this.waveViewerLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waveViewerLayout.Location = new System.Drawing.Point(0, 0);
+            this.waveViewerLayout.Name = "waveViewerLayout";
+            this.waveViewerLayout.RowCount = 5;
+            this.waveViewerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.waveViewerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.waveViewerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.waveViewerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.waveViewerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.waveViewerLayout.Size = new System.Drawing.Size(514, 220);
+            this.waveViewerLayout.TabIndex = 0;
+            // 
+            // waveScope1
+            // 
+            this.waveViewerLayout.SetColumnSpan(this.waveScope1, 3);
+            this.waveScope1.CurrentPosition = 0;
+            this.waveScope1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waveScope1.Location = new System.Drawing.Point(2, 2);
+            this.waveScope1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.waveScope1.MaximumSize = new System.Drawing.Size(750, 120);
+            this.waveScope1.MinimumSize = new System.Drawing.Size(0, 120);
+            this.waveScope1.Name = "waveScope1";
+            this.waveScope1.Size = new System.Drawing.Size(510, 120);
+            this.waveScope1.TabIndex = 0;
+            // 
+            // WaveFormViewer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.toolStripWaveFormViewer);
+            this.Name = "WaveFormViewer";
+            this.Size = new System.Drawing.Size(514, 245);
+            this.toolStripWaveFormViewer.ContentPanel.ResumeLayout(false);
+            this.toolStripWaveFormViewer.ResumeLayout(false);
+            this.toolStripWaveFormViewer.PerformLayout();
+            this.waveViewerLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripWaveFormViewer;
+        private System.Windows.Forms.TableLayoutPanel waveViewerLayout;
+        private WaveReader.WaveScope waveScope1;
+    }
+}
