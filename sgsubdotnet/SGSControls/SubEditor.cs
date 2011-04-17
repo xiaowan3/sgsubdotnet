3fb616f3a9a0859a0a38be15ca913a9c116377c2
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2011-04-17 11:00:33 +0000

    加了一些东西。

--- sgsubdotnet/SGSControls/SubEditor.cs
+++ sgsubdotnet/SGSControls/SubEditor.cs
@@ -15,5 +15,51 @@ namespace SGSControls
         {
             InitializeComponent();
         }
+        #region Private Members
+
+        /// <summary>
+        /// 字幕内容
+        /// </summary>
+        private Subtitle.AssSub m_CurrentSub = new Subtitle.AssSub();
+        private Config.SGSConfig m_Config;
+        #endregion
+
+        #region Events
+        public event EventHandler<SeekEventArgs> Seek = null;
+        public event EventHandler<TimeEditEventArgs> TimeEdit = null;
+        #endregion
+
+        #region Methods
+        public void EditBeginTime(int LineNumber, double Value)
+        {
+        }
+        public void EditEndTime(int LineNumber, double Value)
+        {
+        }
+        #endregion
     }
+
+    public class SeekEventArgs : EventArgs
+    {
+        public double SeekOffset;
+        public SeekDir SeekDirection;
+        public SeekEventArgs(SeekMethod seekdir, double seekoff)
+        {
+            SeekOffset = seekoff;
+            SeekDirection = seekdir;
+        }
+    }
+    public enum SeekDir { Begin, CurrentPos };
+
+    public class TimeEditEventArgs : EventArgs
+    {
+        public TimeType EditTime;
+        public double TimeValue;
+        public TimeEditEventArgs(TimeType editTime, doube timevalue)
+        {
+            EditTime = editTime;
+            TimeValue = timevalue;
+        }
+    }
+    public enum TimeType { BeginTime, EndTime };
 }
