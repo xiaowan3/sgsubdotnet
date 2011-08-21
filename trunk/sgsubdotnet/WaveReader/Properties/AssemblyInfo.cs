75cdc7a463f2cf4fc813046a9a7c2e1a5a8cb552
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2011-08-21 00:51:05 +0000

    update versions.

--- trunk/sgsubdotnet/WaveReader/Properties/AssemblyInfo.cs
+++ trunk/sgsubdotnet/WaveReader/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WaveReader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SGSUB")]
 [assembly: AssemblyProduct("WaveReader")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © SGSUB 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.227")]
+[assembly: AssemblyFileVersion("1.0.1.227")]
