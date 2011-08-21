c0a3140842cb9d6587e5503a944736c0df07ddb0
yantengda <yantengda@9b415766-620d-9d54-d4db-ff58f8b90c40>
2011-08-21 00:35:14 +0000

    update version.

--- trunk/sgsubdotnet/VideoPlayer/Properties/AssemblyInfo.cs
+++ trunk/sgsubdotnet/VideoPlayer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VideoPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("SGSUB")]
 [assembly: AssemblyProduct("VideoPlayer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © SGSUB 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("2.0.1.226")]
+[assembly: AssemblyFileVersion("2.0.1.226")]
